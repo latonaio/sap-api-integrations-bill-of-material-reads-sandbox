a677752c23a873a7e507c550d549b146a157b4ac
MiyuYokoyama0109 <miyu.y@latona.io>
2022-04-29 13:47:24 +0900

    release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-bill-of-material-reads/SAP_API_Caller"
-	"sap-api-integrations-bill-of-material-reads/sap_api_input_reader"
+	"sap-api-integrations-bill-of-material-reads/SAP_API_Input_Reader"
 
 	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
